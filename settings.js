9fc2e4d35049a15eaba591b147230fdc0ddb8837
Dimas-Tokisaki <115859821+Dimas-Tokisaki@users.noreply.github.com>
2023-02-11 10:32:13 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -26,24 +26,24 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "VICTOR BOT" //namabot kalian
-global.ownername= "LEVI WANGYY" //nama kalian
+global.botname = "Kurumj-Bot" //namabot kalian
+global.ownername= "DimasKiyotaka" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
 global.youtube = "https://youtube.com/channel/UC2GOH5mCArh3E4J3sGFMC5g" //bebas asal jan hapus
-global.myweb2 = "https://instagram.com/leviatnansyah?igshid=YmMyMTA2M2Y=" //bebas
-global.email = "leviadnansyahlevi@gmail.com" //bebas
+global.myweb2 = "https://instagram.com/dimas_kiyotaka" //bebas
+global.email = "dimastokisaki494@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['6285172446692'] //premium user
-global.owner = ["6285172446692","6282131369002"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "6282131369002" // nomor wa kalian
-global.ownernomerr = "6285172446692" //nmr wa kalian
+global.premium = ['6283169648444'] //premium user
+global.owner = ["6283169648444","6283169648444"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6283169648444" // nomor wa kalian
+global.ownernomerr = "6289647022414" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© CREATED BY VICTOR BOT' //sticker wm ubah
-global.author = 'Di Buat Oleh LEVI WANGYY' //sticker wm ganti nama kalian
+global.packname = '© CREATED BY Kuruni Bot' //sticker wm ubah
+global.author = 'Di Buat Oleh DimasKiyotaka' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
